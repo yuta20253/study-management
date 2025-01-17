f3ddcb6669854d88d1a66541d232af42dda389e6
yuta20253 <magee.20253@gmail.com>
2025-01-17 14:21:02 +0900

    GitHub Actionsの設定ファイルを追加

--- rails/app/models/like.rb
+++ rails/app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-  belongs_to :to_user, class_name: "User"
-  belongs_to :from_user, class_name: "User"
+  belongs_to :to_user, class_name: "User", foreign_key: "to_user_id"
+  belongs_to :from_user, class_name: "User", foreign_key: "from_user_id"
 
   # 自分が相手にLikeしているかを確認するスコープ
   scope :for_users, ->(from_user, to_user) { where(from_user_id: from_user.id, to_user_id: to_user.id) }
